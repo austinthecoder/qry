a712f93aefc86dfa738cf98423f0f830166ca356
Austin Schneider <me@austinschneider.com>
2023-12-11 20:30:43 -0500

    added minimal hex metadata fields

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Qry.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Simple query language for Elixir.",
+      package: package()
     ]
   end
 
@@ -17,5 +19,12 @@ defmodule Qry.MixProject do
 
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: []
+    ]
   end
 end
