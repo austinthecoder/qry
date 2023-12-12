bc5e1ad30a9159f7b719cc89bc894f68d44252db
Austin Schneider <me@austinschneider.com>
2023-12-12 08:50:13 -0500

    setup test paths

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Qry.MixProject do
       app: :qry,
       version: "0.1.0",
       elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Simple query language for Elixir.",
@@ -17,6 +18,9 @@ defmodule Qry.MixProject do
   def application do
     []
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
