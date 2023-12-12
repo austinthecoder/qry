43a93aa7c4fc8598c100675d45251c45eb63d9e5
Austin Schneider <me@austinschneider.com>
2023-12-11 20:22:54 -0500

    cleaned out generated code

--- lib/qry.ex
+++ lib/qry.ex
@@ -1,18 +1,2 @@
 defmodule Qry do
-  @moduledoc """
-  Documentation for `Qry`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Qry.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/qry_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule QryTest do
-  use ExUnit.Case
-  doctest Qry
-
-  test "greets the world" do
-    assert Qry.hello() == :world
-  end
-end
